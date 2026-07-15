2870bceac8636e23241b33f5123c15b5325211d1
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-07-15 14:19:43 +0700

    add put

--- index.js
+++ index.js
@@ -69,15 +69,35 @@ app.post('/biodata', async (req, res, next) => {
 });
 
 //PUT
+app.put('/biodata/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { nama, nim, kelas } = req.body;
+ 
+        const result = await pool.query(
+            `UPDATE biodata 
+             SET nama = $1, nim = $2, kelas = $3 
+             WHERE id = $4 
+             RETURNING *`,
+            [nama, nim, kelas, id]
+        );
+ 
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Data tidak ditemukan" });
+        }
+ 
+        res.status(200).json({
+            message: "Berhasil mengupdate data biodata",
+            data: result.rows[0]
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Terjadi kesalahan pada server atau database" });
+    }
+});
 
+//DELETE
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
-
-
-//put
-
-//delete
-
-
