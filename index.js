27ad5a131abd9159841d9bdadf13615c30550acf
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2026-07-15 14:37:57 +0700

    all done

--- index.js
+++ index.js
@@ -14,23 +14,9 @@ const pool = new Pool({
 });
  
 app.use(express.json());
-
-app.get('/biodata', async (req, res) => {
-    try {
-        const result = await pool.query("SELECT * FROM biodata");
- 
-        res.status(200).json({
-            message: "Berhasil mengambil data biodata",
-            data: result.rows
-        });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ error: "Terjadi kesalahan pada server atau database" });
-    }
-});
  
 //GET
-app.get('/biodata/:id', (req, res, next) => {
+app.get('/biodata', async (req, res, next) => {
     try {
         const result = await pool.query("SELECT * FROM biodata");
 
